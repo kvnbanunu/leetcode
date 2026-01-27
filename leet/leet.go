9dcce35e02fde06e9df536dd74df0df484d6ae59
kvnbanunu <kvnbanunu@gmail.com>
2026-01-26 17:42:16 -0800

    p15 threesum

--- leet/leet.go
+++ leet/leet.go
@@ -2,7 +2,7 @@ package leet
 
 import "slices"
 
-// p128
+// p128 Longest Consecutive Sequence
 func LongestConsecutive(nums []int) int {
 	if len(nums) == 0 {
 		return 0
@@ -26,6 +26,44 @@ func LongestConsecutive(nums []int) int {
 		streak++
 		curr++
 		res = max(res, streak)
+	}
+	return res
+}
+
+// p15 3Sum
+func ThreeSum(nums []int) [][]int {
+	res := [][]int{}
+
+	slices.Sort(nums)
+
+	for i, v := range nums {
+		// remaining values are positive, no more solutions
+		if v > 0 {
+			break
+		}
+
+		// duplicate
+		if i > 0 && v == nums[i-1] {
+			continue
+		}
+
+		l := i + 1
+		r := len(nums) - 1
+		for l < r {
+			threesum := v + nums[l] + nums[r]
+			if threesum > 0 {
+				r--
+			} else if threesum < 0 {
+				l++
+			} else {
+				res = append(res, []int{v, nums[l], nums[r]})
+				l++
+				r--
+				for l < r && nums[l] == nums[l-1] {
+					l++
+				}
+			}
+		}
 	}
 	return res
 }
